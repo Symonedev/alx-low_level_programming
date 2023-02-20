57b7579a6f24e396876397f14016d688e0622cd9
Symonedev <vincentsymon254@gmail.com>
2023-02-20 06:20:42 -0800

    upper and lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *Description - a program that prints both small and uppercase
-  *Retun: 0
+  *Description: a script thst prints both uppercase and lowercase
+  *Return: always 0
   */
 int main(void)
 {
 	int n = 97;
-	int p = 65;
+	int u = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	while (p <= 90);
+	while (u <= 90)
 	{
-		putchar(p);
-		p++;
+		putchar(u);
+		u++;
 	}
 	putchar('\n');
 	return (0);
 }
-
