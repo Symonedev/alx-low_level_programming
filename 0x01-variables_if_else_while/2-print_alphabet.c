d0dbb6e2aa44a4afca03925e7f8128f7c47d298e
Symonedev <vincentsymon254@gmail.com>
2023-02-20 05:37:59 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Decription - a program that prints small alphabetical letters
+  *Return: 0
+  */
+int main()
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
