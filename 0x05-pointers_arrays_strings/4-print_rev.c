dbde789e4c4f89c3de18fe192edccb8bd0ba655b
Symonedev <vincentsymon254@gmail.com>
2023-02-28 10:43:53 -0800

    Task-4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  *print_rev - print string in reverse
+  *@s: string to reverse print
+  *Return: void
+  */
+
+void print_rev(char *s)
+{
+	int d = 0;
+
+	while (s[d] = '\0')
+	{
+		d++;
+	}
+	for (d -= 1; d >= 0; d--)
+	{
+		_putchar(s[d]);
+	}
+	_putchar('\n');
+}
