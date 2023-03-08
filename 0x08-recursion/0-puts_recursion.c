742453249b6922daf936f5be89ca62babb726ec4
Symonedev <vincentsymon254@gmail.com>
2023-03-08 10:08:36 -0800

    function printing a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,15 @@
   *
   * Return: Nothing.
   */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+     if (*s == '\0')
+     {
+         _putchar('\n');
+         return;
+     }
+    _putchar(*s);
+     s++;
+     _puts_recursion(s);
 }
